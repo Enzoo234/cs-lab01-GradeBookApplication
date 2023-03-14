0e349c60abb4b91fb5afadd03168bc234a69f43f
Krzysztof Wrona <31781455+Enzoo234@users.noreply.github.com>
2023-03-14 16:21:48 +0100

    Fourth Test Completed

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+
+        RankedGradeBook (string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+    }
+}
